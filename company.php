f3827b6f697b2e9fccd94ecdaae50d299fd8ffcd
sethmcleod <me@sethmcleod.com>
2015-05-01 00:52:52 -0400

    Main Pages

---
+++ company.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php $pageTitle = 'Company'; ?>
+    
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/header.php"); ?>
+</head>
+
+<body>
+	
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/nav.php"); ?>
+
+<section class="study">
+    <div class="container">
+        <div class="row">
+            <div class="five columns">
+                <h3>Case Study</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit.</p>
+                <a class="button" href="#">View Case Study</a>
+            </div>
+            <div class="seven columns">
+                <img class="right" src="img/black.gif" alt="Case Study 1" />
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/footer.php"); ?>
+
+</body>
+</html>
