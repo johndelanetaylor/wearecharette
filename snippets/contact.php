380ec88f271d8f372d90a24dc6d913c10bd8761a
sethmcleod <me@sethmcleod.com>
2015-05-07 00:53:48 -0400

    Studio Page

---
+++ snippets/contact.php
@@ -0,0 +1,30 @@
+<form>
+    <div class="row">
+        <div class="six columns">
+            <input class="u-full-width" type="text" name="name" placeholder="Name">
+        </div>
+        <div class="six columns">
+            <input class="u-full-width" type="email" name="email" placeholder="Email">
+        </div>
+    </div>
+    <div class="row">
+        <div class="six columns">
+            <select class="u-full-width" name="budget">
+                <option value="" disabled selected>Budget</option>
+                <option value="1">$100</option>
+                <option value="2">$1,000</option>
+                <option value="3">$10,000</option>
+            </select>
+        </div>
+        <div class="six columns">
+            <select class="u-full-width" name="timeframe">
+                <option value="" disabled selected>Timeframe</option>
+                <option value="1">One Week</option>
+                <option value="2">One Month</option>
+                <option value="3">Six Months</option>
+            </select>
+        </div>
+    </div>
+    <textarea class="u-full-width" placeholder="How can we help you?"></textarea>
+    <input class="button-primary" type="submit" value="Submit">
+</form>
--- studio.php
+++ studio.php
@@ -16,7 +16,7 @@
         <div class="row">
             <div class="twelve columns">
                 <h3>About Us</h3>
-                <h2>We are inspired by the amazing people we get to work</h2>
+                <h2>We are inspired by the amazing people we get to work with</h2>
             </div>
         </div>
     </div> 
@@ -58,6 +58,30 @@
         <div class="row">
             <div class="twelve columns">
                 <h3>Our Process</h3>
+                <!-- Process 1 -->
+                <div class="block">
+                    <span class="dropcap">1</span>
+                    <h4>FIND THE CORE MESSAGE</h4>
+                    <p>Through parallel research, discussion, and exploration, we determine the unique key benefits offered by our client. Who are we? How are we different? What is special about what we’re offering?</p>
+                </div>
+                <!-- Process 2 -->
+                <div class="block">
+                    <span class="dropcap">2</span>
+                    <h4>KNOW THE AUDIENCE</h4>
+                    <p>Using research, intution, discussion and analysis, we determine who it is we are speaking to. Why are we speaking to this group? How can we benefit from communicating with them?</p>
+                </div>
+                <!-- Process 3 -->
+                <div class="block">
+                    <span class="dropcap">3</span>
+                    <h4>CREATE A DIALOG</h4>
+                    <p>Combining the communication objectives with a visual language that speaks to our audience, we create a brand concept that delivers the message in a way that is precise, engaging, and honest.</p>
+                </div>
+                <!-- Process 4 -->
+                <div class="block">
+                    <span class="dropcap">4</span>
+                    <h4>FINE TUNE</h4>
+                    <p>With feedback, discussion, revisions, focus groups and creative problem solving, we dial in our design to make sure it’s on target, clear, and beautiful.</p>
+                </div>
             </div>
         </div>
     </div> 
@@ -70,6 +94,21 @@
             <div class="twelve columns">
                 <h3>We Love Great Company Culture</h3>
                 <h4>And Our Team Rocks!</h4>
+                <div class="item">
+                    <span class="title">Jeff Charette<br>Principal</span>
+                </div>
+                <div class="item">
+                    <span class="title">Emily Charette<br>Principal</span>
+                </div>
+                <div class="item">
+                    <span class="title">John Taylor<br>Designer</span>
+                </div>
+                <div class="item">
+                    <span class="title">Katie Evans<br>Designer</span>
+                </div>
+                <div class="item">
+                    <span class="title">Bianca Borghi<br>Designer</span>
+                </div>
             </div>
         </div>
     </div> 
@@ -82,6 +121,7 @@
             <div class="twelve columns">
                 <h3>Get In Touch</h3>
                 <h4>And Say Hi</h4>
+                <?php include($_SERVER['DOCUMENT_ROOT']."/snippets/contact.php"); ?>
             </div>
         </div>
     </div> 
