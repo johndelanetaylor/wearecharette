ee12fa3dbd05d46121b2a5857cc8b720479b3879
sethmcleod <me@sethmcleod.com>
2015-05-24 18:14:56 -0400

    Natural Sea

---
+++ natural-sea.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php $pageTitle = 'Natural Sea'; ?>
+    
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/header.php"); ?>
+</head>
+
+<body id="natural-sea">
+	
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/nav.php"); ?>
+    
+<!-- Contact Modal -->
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/modal.php"); ?>
+
+<!-- Cover -->
+<section class="cover">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <h1>Natural Sea</h1>
+            </div>
+        </div>
+    </div> 
+</section>
+
+<!-- 8 Col Text -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div class="two columns offset"></div>
+            <div class="eight columns">
+                <h3>The Reel Story</h3>
+                <p>This is the beginning of a beautiful friendship: One fateful day, Emily was pushing her daughter Eleanor (then about 18 months) in a swing at the park, and struck up a conversation with a lovely-looking mom pushing a similarly-aged baby girl nearby. They mostly chatted about childrearing and things to do in St. Augustine (as the other mom was visiting on vacation from New England) but at the very end of the conversation, Emily mentioned that she was a graphic designer. The other mom then introduced Emily to her husband, who works in marketing at Blue Marble Brands, and the rest is history. Six months later, we were starting work on Natural Sea.</p>
+            </div>
+            <div class="two columns offset"></div>
+        </div>
+    </div> 
+</section>
+    
+<!-- Block Quote -->
+<section class="blockquote">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <h2>“The product itself is excellent: sustainably harvested, delicious and nutritious; but the packaging left consumers feeling like they were buying something from the last century.”</h2>
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<!-- 8 Col Text -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div class="two columns offset"></div>
+            <div class="eight columns">
+                <h3>The Refresh</h3>
+                <p>Natural Sea was in dire need of a brand refresh. The product itself is excellent: sustainably harvested, delicious and nutritious; but the packaging left consumers feeling like they were buying something from the last century.</p>
+            </div>
+            <div class="two columns offset"></div>
+        </div>
+    </div> 
+</section>
+    
+<!-- 6 Col Images -->
+<section class="image no-top">
+    <div class="container">
+        <div class="row">
+            <div class="six columns">
+                <img src="/img/natural-sea/before.jpg" />
+            </div>
+            <div class="six columns">
+                <img src="/img/natural-sea/after.jpg" />
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<hr>
+    
+<!-- 8 Col Text -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div class="two columns offset"></div>
+            <div class="eight columns">
+                <h3>Blue Marble</h3>
+                <p>Working with the fantastic team of strategists, art directors, and marketers at Blue Marble, we successfully infused the Natural Sea brand with personality, passion, and freshness using contemporary typography, vivid watercolor illustrations, and lots of spunky messaging. The project included strategy, messaging, brand identity, iconography, web, packaging, promotional materials, social networking and art direction for photography and illustration.</p>
+            </div>
+            <div class="two columns offset"></div>
+        </div>
+    </div> 
+</section>
+    
+    
+<!-- Centered Image -->
+<section class="image no-top">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <img src="/img/natural-sea/logo.jpg" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="twelve columns">
+                <img src="/img/natural-sea/palette.jpg" />
+            </div>
+        </div>
+    </div> 
+</section>
+
+<hr>
+    
+<!-- Slideshow Placeholder -->
+<section class="image">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <img src="/img/natural-sea/slideshow.jpg" />
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<hr>
+    
+<!-- Centered Image -->
+<section class="image">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <a href="http://naturalsea.com" target="_blank"><img src="/img/natural-sea/web.jpg" /></a>
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<hr>
+    
+<!-- Info Text -->
+<section class="text info">
+    <div class="container">
+        <div class="row">
+            <div class="two columns offset"></div>
+            <div class="six columns">
+                <h3>What a Splash!</h3>
+                <p>The updated design brought Natural Sea into the spotlight, and garnered interest from major world-wide retailers. This brand is already on the shelves and moving fast at Target, Whole Foods, and other natural foods retailers.</p>
+            </div>
+            <div class="three columns">
+                <h3>Details</h3>
+                <ul>
+                    <li>Branding</li>
+                    <li>Packaging</li>
+                    <li>Website Design</li>
+                </ul>
+            </div>
+            <div class="one columna offset"></div>
+        </div>
+    </div> 
+</section>
+    
+<!-- Footer -->
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="four columns">
+                <a href="#">
+                    <img src="/img/icons/arrow-left.png" />
+                    <h4>Mediterranean Organic</h4>
+                </a>
+            </div>
+            <div class="four columns">
+                <a href="/work">
+                    <h4>Back to our Projects</h4>
+                </a>
+            </div>
+            <div class="four columns">
+                <a href="#">
+                    <img src="/img/icons/arrow-right.png" />
+                    <h4>City of St. Augustine</h4>
+                </a>
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/footer.php"); ?>
+
+</body>
+</html>
