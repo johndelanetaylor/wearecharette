9a43288cf299c6631be4160c24fc092234770c78
johndelanetaylor <johndelanetaylor@gmail.com>
2015-06-19 15:23:56 -0400

    added tumaros.php page and styles

---
+++ tumaros.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php $pageTitle = 'Tumaros'; ?>
+    
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/header.php"); ?>
+</head>
+
+<body id="tumaros">
+	
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/nav.php"); ?>
+    
+<!-- Contact Modal -->
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/modal.php"); ?>
+
+<!-- Cover -->
+<section class="cover">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <h1>Tumaros</h1>
+            </div>
+        </div>
+    </div> 
+</section>
+
+<!-- 6 Col Text 6 Col Image -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div  class="six columns">
+                <h3>Today's the Day</h3>
+                <p>Tumaro’s is a tortilla brand looking to reposition itself as a wrap brand. They came to We Are Charette hoping to retain current brand equity while taking a bold stand on the deli aisle.</p>
+            </div>
+            <div class="six columns">
+                <img src="/img/tumaros/logo.jpg" />
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<hr>
+
+<!-- Slider Placeholder -->
+<div class="slider color">
+    <div class="slide"><img src="img/tumaros/slide-1.jpg"></div>
+    <div class="slide"><img src="img/tumaros/slide-2.jpg"></div>
+</div>
+    
+    
+<!-- Full Width Images -->
+<section class="full-width-1 image">
+</section>
+
+<!-- 6 Col Text and 6 Col Image -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div class="six columns">
+                <h3>The Brand</h3>
+                <p>This brand is all about the little choices you make every day — trade-ups for your health. To communicate this optimistic message, we developed the tagline “Today’s the day” and a variety of spunky headlines that proudly advertise Tumaro’s as a small change that can do a world of good.  “Say hello to deli-cious”, “let’s skip the sandwich” and “Make the s’wich” help communicate Tumaro’s upbeat spirit and empowering message.</p>
+                <p>Bold colors and strong geometry jump out on shelf and relate Tumaro’s simple message: Today is the day to start making better choices!</p>
+                <br />
+                <h3 class="center pad-bot">Primary Palette</h3>
+                <img class="pad-bot" src="/img/tumaros/colors-1.jpg" />
+                <br />
+                <h3 class="center pad-bot">Secondary Palette</h3>
+                <img src="/img/tumaros/colors-circle.jpg" />
+            </div>
+            <div class="six columns">
+                <img src="/img/tumaros/tortillas-stacked.jpg" />
+            </div>
+        </div>
+    </div>
+</section>
+    
+<!-- Full Width Images -->
+<section class="full-width-2 image">
+</section>
+    
+<!-- Centered Image -->
+<section class="image">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <div class="web-head">
+                    <div class="circles"></div>
+                    <div class="circles"></div>
+                    <div class="circles"></div>
+                </div>
+                <a href="#" target="_blank"><img src="/img/tumaros/web-1.jpg" /></a>
+            </div>
+        </div>
+    </div> 
+</section>   
+    
+<hr>
+    
+<!-- Info Text -->
+<section class="text info">
+    <div class="container">
+        <div class="row">
+            <div class="three columns offset"></div>
+            <div class="four columns">
+                <h3>Details</h3>
+                <p>Branding / Packaging / Web Design</p>
+            </div>
+            <div class="three columns">
+                <h3>View It Live</h3>
+                <p><a href="#">Tumaros.com</a></p>
+            </div>
+            <div class="two columns offset"></div>
+        </div>
+    </div> 
+</section>
+    
+<!-- Footer -->
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="four columns">
+                <a href="#">
+                    <img src="/img/icons/arrow-left.png" />
+                    <h4>Mediterranean Organic</h4>
+                </a>
+            </div>
+            <div class="four columns">
+                <a href="/work">
+                    <h4>Back to our Projects</h4>
+                </a>
+            </div>
+            <div class="four columns">
+                <a href="#">
+                    <img src="/img/icons/arrow-right.png" />
+                    <h4>City of St. Augustine</h4>
+                </a>
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/footer.php"); ?>
+
+</body>
+</html>
