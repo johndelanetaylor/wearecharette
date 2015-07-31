5984f2da408cd07311ff2328b11990909984be5e
johndelanetaylor <johndelanetaylor@gmail.com>
2015-07-31 17:02:48 -0400

    added harvest bay page to queue

---
+++ harvest-bay.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang='en'>
+<?php $pageTitle = 'Harvest Bay'; ?>
+    
+<head>
+	<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/header.php"); ?>
+</head>
+
+<body id="harvest-bay">
+	
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/nav.php"); ?>
+    
+<!-- Contact Modal -->
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/modal.php"); ?>
+
+<!-- Cover -->
+<section class="cover">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns">
+                <h1>Harvest Bay</h1>
+            </div>
+        </div>
+    </div> 
+</section>
+
+<!-- 6 Col Text 6 Col Image -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div  class="six columns">
+                <h3>Natural Refreshment Refresh</h3>
+                <p>This popular coconut water brand was extending their line to include three new certified organic flavors. To help them launch their new products, We Are Charette created an adaptation of the Harvest Bay Coconut Water lockup to include “Organic”, and we created three new package designs focusing on the indulgent flavors and premium ingredients of each sku.</p>
+            </div>
+            <div class="six columns">
+                <img src="/img/harvest-bay/harvest-bay-logo.jpg" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="six columns">
+                <h3>Details</h3>
+                <p>Branding / Packaging / Illustration</p>
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<hr>
+
+<!-- Slider Placeholder -->
+<div class="slider color">
+    <div class="slide"><img src="img/harvest-bay/slide-1.jpg"></div>
+    <div class="slide"><img src="img/harvest-bay/slide-2.jpg"></div>
+    <div class="slide"><img src="img/harvest-bay/slide-3.jpg"></div>
+    <div class="slide"><img src="img/harvest-bay/slide-4.jpg"></div>
+</div>
+
+<hr>
+    
+<!-- 8 Col Text -->
+<section class="text">
+    <div class="container">
+        <div class="row">
+            <div class="two columns offset"></div>
+            <div class="eight columns">
+                <h3>The Illustrations</h3>
+                <p>Harvest Bay already had some great looking illustrations, and our client wanted this new line to feel cohesive with the original branding. We were tasked with creating new sku illustrations that used a consistent style while adding a bit of sophistication for this more indulgent line. The resulting illustrations showcase the natural ingredients and use clean vector styles with the added juiciness of warm, vibrant gradients.</p>
+            </div>
+            <div class="two columns offset"></div>
+        </div>
+    </div> 
+</section>
+    
+<!-- 12 Col Images -->
+<section class="image">
+    <div class="container">
+        <div class="row">
+            <div class="twelve columns center">
+                <img src="/img/harvest-bay/harvest-bay-illustration-1.jpg" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="twelve columns center">
+                <img src="/img/harvest-bay/harvest-bay-illustration-2.jpg" />
+            </div>
+        </div>
+        <div class="row">
+            <div class="twelve columns center">
+                <img src="/img/harvest-bay/harvest-bay-illustration-3.jpg" />
+            </div>
+        </div>
+    </div>
+</section>
+    
+<!-- Footer -->
+<section class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="four columns">
+                <a href="/tumaros">
+                    <img src="/img/icons/arrow-left.png" />
+                    <h4>Tumaros</h4>
+                </a>
+            </div>
+            <div class="four columns">
+                <a href="/work">
+                    <h4>Back to our Projects</h4>
+                </a>
+            </div>
+            <div class="four columns">
+                <a href="/happy-cup">
+                    <img src="/img/icons/arrow-right.png" />
+                    <h4>Happy Cup</h4>
+                </a>
+            </div>
+        </div>
+    </div> 
+</section>
+    
+<?php include($_SERVER['DOCUMENT_ROOT']."/snippets/footer.php"); ?>
+
+</body>
+</html>
